50e4e287212cc087df7a2fa3dee91222df1447b2
Mokhtar Emad <98131830+mokhtaa@users.noreply.github.com>
2025-07-20 18:07:58 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
         cvResult.innerHTML = '<div class="loading-spinner"></div><p class="placeholder-text">يتم الآن تحليل بياناتك بواسطة الذكاء الاصطناعي...</p>';
 
         try {
-            const response = await fetch('http://mokhtar.pythonanywhere.com/', {
+            const response = await fetch('https://mokhtar.pythonanywhere.com/', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
